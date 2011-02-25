078dcb8b814906805d9ba765bae2cdc57290bb4c
sazarkevich <sazarkevich@118a7941-db5d-044e-b307-4b50267aae01>
2011-02-25 07:28:05 +0000

    add: new plugins loader
    git-svn-id: svn://azarkevich/trunk/phpBBx Helper@28 118a7941-db5d-044e-b307-4b50267aae01

--- src/content/plugins-manager.js
+++ src/content/plugins-manager.js
@@ -1,86 +1,9 @@
-function phpBBx_getContentDir()
-{
-	var info = Components.classes["@mozilla.org/xre/app-info;1"]
-		.getService(Components.interfaces.nsIXULAppInfo);
-
-	if(/^4\.*/.test(info.version))
-		return phpBBx_getContentDir4();
-	else
-		return phpBBx_getContentDir3();
-}
-
-function phpBBx_getContentDir4()
-{
-	var directoryService = Components.classes["@mozilla.org/file/directory_service;1"].
-		getService(Ci.nsIProperties);
-
-	var dir = directoryService.get("ProfD", Components.interfaces.nsIFile);
-	dir.append("extensions");
-	dir.append("phpBBx-helper@sergey.azarkevich");
-	dir.append("content");
-	
-	return dir;
-}
-
-function phpBBx_getContentDir3()
-{
-	var il = Components.classes["@mozilla.org/extensions/manager;1"]
-			   .getService(Components.interfaces.nsIExtensionManager)
-			   .getInstallLocation("phpBBx-helper@sergey.azarkevich");
-	var dir = il.location;
-	dir.append("phpBBx-helper@sergey.azarkevich");
-	if(dir.isFile())
-	{
-		// open an input stream from file
-		var istream = Components.classes["@mozilla.org/network/file-input-stream;1"]
-			.createInstance(Components.interfaces.nsIFileInputStream);
-		istream.init(dir, 0x01, 0444, 0);
-		istream.QueryInterface(Components.interfaces.nsILineInputStream);
-
-		// read lines into array
-		var line = {}, lines = [], hasmore;
-		do {
-		  hasmore = istream.readLine(line);
-		  lines.push(line.value); 
-		} while(hasmore);
-
-		istream.close();
-
-		// do something with read data
-		dir = Components.classes["@mozilla.org/file/local;1"].
-			createInstance(Components.interfaces.nsILocalFile);
-		dir.initWithPath(lines[0]);
-	}
-
-	dir.append("content");
-	
-	return dir;
-}
-
-function phpBBx_getPlugins()
-{
-	var dir = phpBBx_getContentDir();
-
-	dir.append("plugins");
-	
-	var entries = dir.directoryEntries;
-	var array = [];
-	while(entries.hasMoreElements())
-	{
-	  var entry = entries.getNext();
-	  entry.QueryInterface(Components.interfaces.nsIFile);
-	  array.push(entry);
-	}
-	
-	return array;
-}
-
 function phpBBx_loadPlugins()
 {
 	var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
 		.getService(Components.interfaces.mozIJSSubScriptLoader);
 
-	var array = phpBBx_getPlugins();
+	var array = phpBBx_getPluginUrls();
 	var plugins = new Array();
 	for(var i=0;i<array.length;i++)
 	{
---
+++ src/content/plugins/index.js
@@ -0,0 +1,23 @@
+function phpBBx_getPluginUrls()
+{
+	var array = [];
+	
+	array.push('chrome://phpBBx-helper/content/plugins/cut-long-urls.js');
+	array.push('chrome://phpBBx-helper/content/plugins/explain-card-info.js');
+	array.push('chrome://phpBBx-helper/content/plugins/fix-new-last-x-hours.js');
+	array.push('chrome://phpBBx-helper/content/plugins/fix-smilies.js');
+	array.push('chrome://phpBBx-helper/content/plugins/ignore-users');
+	array.push('chrome://phpBBx-helper/content/plugins/open-all-pages.js');
+	array.push('chrome://phpBBx-helper/content/plugins/posting.js');
+	array.push('chrome://phpBBx-helper/content/plugins/print-2-view.js');
+	array.push('chrome://phpBBx-helper/content/plugins/remove-excl.js');
+	array.push('chrome://phpBBx-helper/content/plugins/replace-smileys');
+	array.push('chrome://phpBBx-helper/content/plugins/search_me.js');
+	array.push('chrome://phpBBx-helper/content/plugins/shortcuts.js');
+	array.push('chrome://phpBBx-helper/content/plugins/tab_number.js');
+	array.push('chrome://phpBBx-helper/content/plugins/tabicon.js');
+	array.push('chrome://phpBBx-helper/content/plugins/ignore-users/plugin.js');
+	array.push('chrome://phpBBx-helper/content/plugins/replace-smileys/plugin.js');
+	
+	return array;
+}
